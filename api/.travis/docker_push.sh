688c9503f047d86332992f97275753a6873a7c9b
Jon Minter <jdiminter@gmail.com>
2018-05-19 19:59:28 -0400

    Fixing travis login to AWS ECR, and fixing env var declaration for IMAGE_NAME

--- api/.travis/docker_push.sh
+++ api/.travis/docker_push.sh
@@ -7,13 +7,16 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     # This is needed to login on AWS and push the image on ECR
     # Change it accordingly to your docker repo
     pip install --user awscli
-    export PATH=$PATH:$HOME/.local/bin
+    export PY_USER_BIN=$(python -c 'import site; print(site.USER_BASE + "/bin")')
+    export PATH=$PATH:$PY_USER_BIN
     eval $(aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION)
 
     # Build and push
+    echo "Building IMAGE_NAME=$IMAGE_NAME"
     docker build -t $IMAGE_NAME .
-    echo "Pushing $IMAGE_NAME:latest"
+    echo "Tagging $IMAGE_NAME:latest $REMOTE_IMAGE_URL:latest"
     docker tag $IMAGE_NAME:latest "$REMOTE_IMAGE_URL:latest"
+    echo "Pushing $IMAGE_NAME:latest"
     docker push "$REMOTE_IMAGE_URL:latest"
     echo "Pushed $IMAGE_NAME:latest"
   else
